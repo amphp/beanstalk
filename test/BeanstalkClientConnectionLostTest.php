719f8a262520a6c33e695da0ec8a2af70342d5cc
user <ekhvalov@gmail.com>
2019-12-24 15:21:36 +0900

    BeanstalkClientConnectionLostTest added.

---
+++ test/BeanstalkClientConnectionLostTest.php
@@ -0,0 +1,75 @@
+<?php
+namespace Amp\Beanstalk\Test;
+
+use Amp\Beanstalk\BeanstalkClient;
+use Amp\Delayed;
+use Amp\PHPUnit\AsyncTestCase;
+use Amp\Socket\Server;
+use Amp\Socket\SocketException;
+use function Amp\call;
+use function Amp\Promise\all;
+use function Amp\Promise\timeout;
+use function Amp\Socket\listen;
+
+class BeanstalkClientConnectionLostTest extends AsyncTestCase
+{
+    private const PORT_RANGE_MIN = 50000;
+    private const PORT_RANGE_MAX = 65535;
+
+    /** @var Server */
+    private $server;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->server = $this->initServer();
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+        $this->server->close();
+    }
+
+    /**
+     * @dataProvider dataProviderReserve
+     * @expectedException \Amp\Beanstalk\ConnectionLostException
+     *
+     * @param $reserveTimeout int|null Seconds
+     * @param $connectionCloseTimeout int Milliseconds
+     * @param $testFailTimeout int Milliseconds
+     * @return \Generator
+     */
+    public function testReserve($reserveTimeout, $connectionCloseTimeout, $testFailTimeout)
+    {
+        $beanstalk = new BeanstalkClient(sprintf('tcp://%s', $this->server->getAddress()));
+        $connectionClosePromise = call(function ($connectionCloseTimeout) {
+            yield new Delayed($connectionCloseTimeout);
+            $this->server->close();
+        }, $connectionCloseTimeout);
+
+        yield timeout(all([
+            $beanstalk->reserve($reserveTimeout),
+            $connectionClosePromise
+        ]), $testFailTimeout);
+    }
+
+    public function dataProviderReserve(): array
+    {
+        return [
+            'no timeout' => [null, 500, 600],
+            'one second timeout' => [1, 900, 1100],
+        ];
+    }
+
+    private function initServer(): Server
+    {
+        for ($port = self::PORT_RANGE_MIN; $port <= self::PORT_RANGE_MAX; $port++) {
+            try {
+                return listen(sprintf('tcp://127.0.0.1:%d', $port));
+            } catch (SocketException $e) {
+            }
+        }
+        throw new \RuntimeException('No available port found');
+    }
+}
