cb1dba74ccbec78c1574195f5c227ce38cefa283
Bilge <bilge@scriptfusion.com>
2022-10-18 20:57:35 +0100

    Updated examples to be Amp v3 (fibers) compliant.

--- examples/producer.php
+++ examples/producer.php
@@ -3,21 +3,18 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Amp\Beanstalk\BeanstalkClient;
-use Amp\Loop;
 
-Loop::run(function () {
-    $beanstalk = new BeanstalkClient("tcp://127.0.0.1:11300");
-    yield $beanstalk->use('foobar');
+$beanstalk = new BeanstalkClient('tcp://127.0.0.1:11300');
+$beanstalk->use('foobar');
 
-    $payload = json_encode([
-        "job" => bin2hex(random_bytes(16)),
-        "type" => "compress-image",
-        "path" => "/path/to/image.png"
-    ]);
+$payload = json_encode([
+    'job' => bin2hex(random_bytes(16)),
+    'type' => 'compress-image',
+    'path' => '/path/to/image.png'
+]);
 
-    $jobId = yield $beanstalk->put($payload);
+$jobId = $beanstalk->put($payload);
 
-    echo "Inserted job id: $jobId\n";
+echo "Inserted job id: $jobId\n";
 
-    $beanstalk->quit();
-});
+$beanstalk->quit();
